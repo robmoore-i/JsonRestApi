c607e8dd8b4bbd457042a46f22a52bcdac51fd9b
Rob <robmoore121@gmail.com>
2018-11-09 00:13:20 +0000

    rest_test is now a buildable module from github.

---
+++ build.sh
@@ -0,0 +1,2 @@
+# Install rest_test for the integration tests.
+pip3 install git+git://github.com/robknows/rest_test.git#egg=rest_test
--- test/test.py
+++ test/test.py
@@ -1,7 +1,16 @@
+import os
 import requests
 
-from resttest import *
+from rest_test import *
 from assertpy import assert_that
+
+QHOME = os.environ["QHOME"]
+
+def start_backend():
+  os.system(QHOME + "/l32/q backend.q &")
+
+def stop_backend():
+  os.system("pkill -f \"" + QHOME + "/l32/q backend.q\"")
 
 @test
 def default_path():
@@ -74,5 +83,5 @@ def path_args_with_cookies():
   assert_that(res.status_code).is_equal_to(200)
   assert_that(res.json()).is_equal_to("pathargs -> one -> two")
 
-main(locals())
+main(locals(), start_backend, stop_backend)
 exit(0)
