23d3440914260c0438e3616ec8f3c786df74fee1
robknows <robmoore121@gmail.com>
2018-11-13 16:06:41 +0000

    upgrade rest_test on build

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 # Install rest_test for the integration tests.
-pip3 install git+git://github.com/robknows/rest_test.git#egg=rest_test
+pip3 install git+git://github.com/robknows/rest_test.git#egg=rest_test --user --upgrade
